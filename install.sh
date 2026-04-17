7c9b478c6b7c7fde70552461740b10a6352d6348
jackieclawhorn <jackieclawhorn@proton.me>
2026-04-16 18:19:01 -0700

    Run install as current user, only sudo systemd steps

--- install.sh
+++ install.sh
@@ -2,8 +2,7 @@
 set -e
 
 REPO="https://github.com/monkbuddy62/plant-timelapse-pi.git"
-REAL_USER="$(logname 2>/dev/null || echo "${SUDO_USER:-$USER}")"
-INSTALL_DIR="/home/$REAL_USER/plant-timelapse"
+INSTALL_DIR="$HOME/plant-timelapse"
 
 echo "Cloning plant-timelapse-pi into $INSTALL_DIR..."
 git clone "$REPO" "$INSTALL_DIR"
@@ -14,7 +13,7 @@ python3 -m venv "$INSTALL_DIR/venv"
 "$INSTALL_DIR/venv/bin/pip" install -r requirements.txt
 
 echo "Installing systemd service..."
-sed "s|__INSTALL_DIR__|$INSTALL_DIR|g; s|__USER__|$REAL_USER|g" plant-timelapse.service | sudo tee /etc/systemd/system/plant-timelapse.service > /dev/null
+sed "s|__INSTALL_DIR__|$INSTALL_DIR|g; s|__USER__|$USER|g" plant-timelapse.service | sudo tee /etc/systemd/system/plant-timelapse.service > /dev/null
 sudo systemctl daemon-reload
 sudo systemctl enable plant-timelapse
 sudo systemctl start plant-timelapse
