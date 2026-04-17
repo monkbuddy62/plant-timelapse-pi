f0379efc4cc3903cb4c0475aa3e93cd9b5ecea95
jackieclawhorn <jackieclawhorn@proton.me>
2026-04-16 18:15:08 -0700

    Add install.sh for Pi setup

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+REPO="https://github.com/monkbuddy62/plant-timelapse-pi.git"
+INSTALL_DIR="/home/pi/plant-timelapse"
+
+echo "Cloning plant-timelapse-pi..."
+git clone "$REPO" "$INSTALL_DIR"
+cd "$INSTALL_DIR"
+
+echo "Installing Python dependencies..."
+pip3 install -r requirements.txt
+
+echo "Installing systemd service..."
+sudo cp plant-timelapse.service /etc/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl enable plant-timelapse
+sudo systemctl start plant-timelapse
+
+echo "Done! Service status:"
+sudo systemctl status plant-timelapse --no-pager
