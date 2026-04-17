f6491c019777ec52aa4a9960d146c706d3743b58
jackieclawhorn <jackieclawhorn@proton.me>
2026-04-16 18:16:29 -0700

    Make install paths user-agnostic using $HOME/$USER

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 set -e
 
 REPO="https://github.com/monkbuddy62/plant-timelapse-pi.git"
-INSTALL_DIR="/home/pato/plant-timelapse"
+INSTALL_DIR="$HOME/plant-timelapse"
 
 echo "Cloning plant-timelapse-pi..."
 git clone "$REPO" "$INSTALL_DIR"
@@ -12,7 +12,7 @@ echo "Installing Python dependencies..."
 pip3 install -r requirements.txt
 
 echo "Installing systemd service..."
-sudo cp plant-timelapse.service /etc/systemd/system/
+sed "s|__INSTALL_DIR__|$INSTALL_DIR|g; s|__USER__|$USER|g" plant-timelapse.service | sudo tee /etc/systemd/system/plant-timelapse.service > /dev/null
 sudo systemctl daemon-reload
 sudo systemctl enable plant-timelapse
 sudo systemctl start plant-timelapse
